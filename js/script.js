0c30ef07ef5eb965b2e1281823c9b47f7e5028ce
Lea <553344777@qq.com>
2022-02-28 22:20:14 +0800

    稍作修改优化。

--- js/script.js
+++ js/script.js
@@ -1,11 +1,57 @@
-var x = 0;
+$(() => {
+    $('.m-nav_c').hide();
+    // 动态添加元素
+    var title = "Lea's Homepage"
+    for (let i = 0; i < title.length; i++) {
+        var span = document.createElement('span')
+        if (title.charAt(i) == ' ') {
+            $('#title').append('<br>')
+            continue
+        }
+        span.innerText = title.charAt(i)
+        span.className = 'flip'
+        $('#title').append(span)
 
-function mnav() {
-    if (x == 0) {
-        document.getElementById("m-nav_c").style.display = "flex";
-        x = 1
-    } else {
-        document.getElementById("m-nav_c").style.display = "none";
-        x = 0
     }
-}
+    // 翻转动画
+    document.querySelectorAll('#title span').forEach((span, index) => {
+        span.style.setProperty('--delay', `${index * 0.1}s`)
+    });
+    // 打字效果
+    let d = $('#text')
+    let i = 0,
+        line = 0;
+    str = ['Never forget to think!', '不积跬步，无以至千里；不积小流，无以成江海。', '世间本无事，庸人自扰之。', '好记性不如烂笔头。', '愿你走出半生，归来仍是少年。'];
+    // 输入
+    function typing() {
+        if (i < str[line].length) {
+            d.html(str[line].slice(0, i++))
+            setTimeout(typing, 200 + Math.ceil(Math.random() * 100))
+        } else {
+            d.html(str[line])
+            setTimeout(re_typing, 2000)
+        }
+    }
+    // 删除
+    function re_typing() {
+        if (i > 0) {
+            d.html(str[line].slice(0, i--))
+            setTimeout(re_typing, 50)
+        } else {
+            if (line == str.length - 1) {
+                line = 0
+            } else {
+                line += 1
+            }
+            d.html('')
+            setTimeout(typing, 1000)
+        }
+    }
+
+    typing();
+
+
+    $('.btn').click(() => {
+        $('.m-nav_c').slideToggle();
+    })
+})
